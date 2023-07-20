c8c3586d35ac419c945985ab86fc1c9135d4dfd1
Lul-Abdifan <tasanbannu@gmail.com>
2023-07-20 08:27:02 -0400

    Update likes schema table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_060826) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_20_121825) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,18 +28,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_060826) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "post_id", null: false
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "post_id", null: false
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -53,9 +53,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_060826) do
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
-  add_foreign_key "comments", "authors", column: "user_id"
+  add_foreign_key "comments", "authors"
   add_foreign_key "comments", "posts"
-  add_foreign_key "likes", "authors", column: "user_id"
+  add_foreign_key "likes", "authors"
   add_foreign_key "likes", "posts"
   add_foreign_key "posts", "authors"
 end
