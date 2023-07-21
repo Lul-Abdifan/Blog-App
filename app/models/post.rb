073745fb13c38a75738027be3b0191578214234a
Lul-Abdifan <tasanbannu@gmail.com>
2023-07-21 04:40:58 -0400

    Add five_recents_comment function

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes
   has_many :comments
+
+  def posts_counter_function
+    author.increment!(:posts_counter)
+  end
+
+  def five_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
