ffe8896b329483bba3fead9a83c572bc8722f71a
Lul-Abdifan <tasanbannu@gmail.com>
2023-07-22 04:33:21 -0400

    Testing for user model

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -20,4 +20,20 @@ RSpec.describe User, type: :model do
         user = User.new(photo:"abdi_photo.jpeg",bio:"I am just nobody")
          expect(user).not_to be_valid
     end
+
+    it "returns up to 3 recent posts" do
+        first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                                 bio: 'Teacher from Mexico.')
+        recent_posts = first_user.recent_posts
+  
+        # Expecting the length of recent_posts to be 0 since no posts have been created yet
+        expect(recent_posts.length).to eq(0)
+  
+        Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+        Post.create(author: first_user, title: 'Hello', text: 'This is my second post')
+        Post.create(author: first_user, title: 'Hello', text: 'This is my third post')
+  
+       
+        expect(first_user.recent_posts.length).to eq(3)
+        end
 end
