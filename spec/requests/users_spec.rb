09d147fffd56b308b853bc6cf3befb3ed3fa2219
Lul-Abdifan <tasanbannu@gmail.com>
2023-07-25 10:12:03 -0400

    Test posts request

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
+   describe 'GET /index' do
+
+    it "successful status" do
+      get '/users/index'
+      expect(response).to have_http_status(:success)
+      
+    end
+
+    it "rendering a correct template" do
+      get '/users'
+      expect(response).to render_template(:index)
+      
+    end
+    it 'Corrent placeholder in body' do
+      get '/users'
+      expect(response.body).to include('Lists of user')
+    end
+   end
+
+
+
+  describe 'GET /show' do
+
+    it "users status" do
+      get '/users/1'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the show template' do
+      get '/users/1'
+      expect(response).to render_template(:show)
+    end
+
+    it 'Corrent placeholder in body' do
+      get '/users/1'
+      expect(response.body).to include('Here is individual user')
+    end
+end
 end
