22e421ccbda7cfe74dec713950d211375917c7e4
Lul-Abdifan <tasanbannu@gmail.com>
2023-07-25 10:12:27 -0400

    Test userss request

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,41 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
+
+    it "successful status" do
+      get '/users/1/posts'
+      expect(response).to have_http_status(:success)
+      
+    end
+
+    it "rendering a correct template" do
+      get '/users/1/posts'
+      expect(response).to render_template(:index)
+      
+    end
+    it 'Corrent placeholder in body' do
+      get '/users/1/posts'
+      expect(response.body).to include('All posts for individual author are here')
+    end
+   end
+
+
+
+  describe 'GET /index ' do
+
+    it "users status" do
+      get '/users/1/posts/1'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the show template' do
+      get '/users/1/posts/1'
+      expect(response).to render_template(:show)
+    end
+
+    it 'Corrent placeholder in body' do
+      get '/users/1/posts/1'
+      expect(response.body).to include('individual post is here')
+    end
+end
 end
