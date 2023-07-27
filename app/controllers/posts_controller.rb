87364329380eb8e24951db3f7f78dd34eeb0c1f4
Lul-Abdifan <tasanbannu@gmail.com>
2023-07-27 06:10:18 -0400

    Fix eslint errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,10 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-  
-   
-   end
+  end
 
   def show
     @post = Post.find(params[:id])
-    @comments =@post.five_recent_comments
-   end
+    @comments = @post.five_recent_comments
+  end
 end
