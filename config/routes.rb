31b604ac69c0c648586b380ca8a026771f59862c
Lul-Abdifan <tasanbannu@gmail.com>
2023-07-29 05:00:19 -0400

    Add post comment functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
  
   root "users#index"
   resources :users, only: [:show, :index] do
-       resources :posts, only: [:show, :index,:new,:create]
+       resources :posts, only: [:show, :index,:new,:create] do
+               resources :comments, only: [:new, :create]
+               resources :likes,only: [:create]
+       end
+       
   end
  
 
