89eee687a7e2fd64ee0d594312db9984eed157bc
Lul-Abdifan <tasanbannu@gmail.com>
2023-07-29 06:35:33 -0400

    Add like posts functionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,12 @@
 class LikesController < ApplicationController
+   def create
+        @like = Like.new
+        @like.author_id =current_user.id
+        @like.post_id = params[:post_id]
+        if @like.save
+            flash[:success] = 'Your like is saved!'
+            redirect_to(request.referer)
+        end
+    end
+
 end
