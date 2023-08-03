70d929aeef16477952898bef73642178b54c44db
Lul-Abdifan <tasanbannu@gmail.com>
2023-08-03 13:45:36 -0400

    Add role input on registration page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,18 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes, foreign_key: :author_id
+  before_save :downcase_role
 
-  validates :name, presence: true
+  validates :name,:role, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  private
+
+  def downcase_role
+    self.role = role.downcase if role.present?
   end
 end
