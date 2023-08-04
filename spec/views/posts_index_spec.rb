d8a2a574c5d99c490a2158e21f32ecab1cd7602a
Lul-Abdifan <tasanbannu@gmail.com>
2023-08-04 10:34:35 -0400

    Add post index integration testing

---
+++ spec/views/posts_index_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe 'Integration Testin', type: :system do
+    describe "Users index page" do
+
+   before(:each) do
+
+    end
+   it "user's profile picture" do
+     end
+
+     it "user's username" do
+    expect(page).to have_content(@user1.name)
+    end
+
+    it "User Profile Picture" do
+        expect(page).to have_css("img[src='#{@user1.photo}']")
+       end
+
+       it "number of posts" do 
+
+              expect(page).to have_content(@user1.posts_counter)
+       end
+
+       it ' posts title' do
+  
+          expect(page).to have_content(@post1.text)
+       end
+
+       it 'shows posts body.' do
+         expect(page).to have_content(@post1.text)
+      end
+
+      it "first comments on a post " do
+          expect(page).to have_content(@user1.posts[0].comments[0])
+      end
+
+      it " how many comments a post has." do
+          expect(page).to have_content(@user1.posts[0].comments_counter)
+      end
+
+      it " how many likes a post has" do
+          expect(page).to have_content(@user1.posts[0].likes_counter)
+      end
+
+      it "section for pagination" do
+              expect(page).to have_link('Pagination', class: 'pagination')
+      end
+
+      it "click on a post, it redirects me to that post's show page" do
+    
+        click_link(@post1.text)
+        expect(page).to have_current_path(user_post_path(@user1, @post1))
+      end
+       
+
+    end
+end
--- spec/views/users_show_spec.rb
+++ spec/views/users_show_spec.rb
@@ -1,54 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe 'Integration Testin', type: :system do
-    describe "Users index page" do
+    describe "Users show page" do
         before(:each) do
             @user1 = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                 bio: 'Teacher from Mexico.')
            @post = Post.create(author: @user1, title: 'Hello', text: 'This is my first post')
             Post.create(author: @user1, title: 'yup', text: 'This is my second post')
             Post.create(author: @user1, title: 'Bye', text: 'This is my third post')
-            end
+           
+            visit user_path(@user1)
+
+
+          end
                it "User Profile Picture" do
-                  visit user_path(@user1)
+
                 expect(page).to have_css("img[src='#{@user1.photo}']")
                end
 
                it "Check the user's username" do
-                visit user_path(@user1)
                 expect(page).to have_content(@user1.name)
                end
 
                it "Number of posts the user" do
-                visit user_path(@user1)
                 expect(page).to have_content(@user1.posts_counter)
                end
 
                it "User's Bio" do
-                visit user_path(@user1)
                 expect(page).to have_content(@user1.bio)
                end
 
                it "Three recent posts" do
-                visit user_path(@user1)
                   expect(@user1.recent_posts.length).to eq 3
                end
 
                it "has a link to the user's posts page" do
-                  visit user_path(@user1)
+
                           expect(page).to have_link('See all Posts', href: user_posts_path(@user1), class: 'all_post_btn')
               end
 
               it "click a user's post, it redirects to that post's show page" do
-                   visit user_path(@user1)
+
                  click_link('See all Posts')
                      expect(page).to have_current_path(user_posts_path(@user1))
             end
 
             it "redirects to the user's post's index page" do
-                visit user_path(@user1)
                 click_link('See all Posts')
-                click_link(@post.title)
+                click_link(@post.text)
                  expect(page).to have_current_path(user_post_path(@user1, @post))
             end
                
