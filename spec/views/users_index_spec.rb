5ca7b57bbc65a56896d284aaab2bcfb98cc23a6d
Lul-Abdifan <tasanbannu@gmail.com>
2023-08-04 03:02:46 -0400

    Add users index integration testing

--- spec/views/users_index_spec.rb
+++ spec/views/users_index_spec.rb
@@ -13,6 +13,28 @@ RSpec.describe 'Hello world', type: :system do
         expect(page).to have_content(@user1.name)
       end
 
+      
+      it "Display Users photo" do
+        visit "/users"
+        all('.custom-list-item').each_with_index do |list_item, index|
+          user = instance_variable_get("@user#{index + 1}")
+          expect(list_item).to have_css("img[src='#{user.photo}']")
+      end
+    end
+
+
+      it "Number of post for users" do
+        visit "/users"
+        expect(page).to have_content(@user1.posts_counter)
+      end
+
+      it "Clicking the user redirects to its show page" do
+        visit "/users"
+        click_link "#{@user1.name}"
+
+             expect(page).to have_content(@user1.bio)
+
+      end
       end
    
 end
